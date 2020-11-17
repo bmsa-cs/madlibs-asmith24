cf346cb7f9716f56897c2daf5eddef04f9b811c3
Alana Smith <asmith24@biomedscienceacademy.org>
2020-11-17 15:48:28 +0000

    updated code + submission

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+"""" 
+Mad Libs 
+Core 6 
+Alana Smith 
 """
-MadLibs
-Author: 
-Period/Core:
+
+print("Lets play Mad Libs! It will be fun :-)")
 
 
-"""
+name = input("choose a name:")
+
+adj = input("choose an adjective:")
+
+prnn = input("choose a pronoun -herself + hiself + themself etc-:")
+ 
+food = input("choose a food:")
+
+adverb = input("choose an adverb:")
+
+place = input("choose a place:")
+
+verb = input("choose an action verb:")
+
+friend = input("choose a friends name:")
+
+print("\n Today," + name + " was having a very " + adj + " day." )
+print("After a long day of feeling "+ adj + ", " + name + " wanted to have their favorite food," + food +".")
+
+print("\n I really want " + food + ", " + name + " thought to " + prnn + ".")
+print("There is a " + adverb + " " + food + " place in " + place + ". Lets go!")
+
+print( "Well, " + name + " should bring a friend! So," + friend + " and I started " + verb + " to the restraunt in" + place)
+print (" The " + food + " was " + adj + " as always.")
